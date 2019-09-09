85366eb40a6054d61ec95b3248bdd1bde2cc2f35
Jack Legate <jblegate@fvhx2d72j1wk.emp.bv>
2019-09-09 13:51:57 -0500

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Jack B. Legate on 9/9/19.
+//  Copyright © 2019 Jack B. Legate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
