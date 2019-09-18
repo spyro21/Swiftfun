4270fb254f282b9617b8b111c4444661953e83ca
Jack Legate <jacklegate21@gmail.com>
2019-09-18 14:03:42 -0500

    Lecture 20

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -6,9 +6,20 @@ class ViewController: UIViewController {
     var numTapCount = 0
     
     @IBOutlet weak var MyLabel: UILabel!
+    @IBOutlet weak var TopTextField: UITextField!
+    @IBOutlet weak var BottomTextField: UITextField!
     
     @IBAction func ButtonTapped(_ sender: Any) {
-        print(numTapCount)
+     
+        
+        var sum = Double(TopTextField.text!)! + Double(BottomTextField.text!)!
+        
+        MyLabel.text = "\(TopTextField.text!) + \(BottomTextField.text!) = \(sum)"
+        
+        
+        
+        
+        
         numTapCount = numTapCount + 1
         if numTapCount == 5{
         print(numTapCount)
